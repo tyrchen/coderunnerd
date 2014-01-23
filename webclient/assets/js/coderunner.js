ea35d9d9121bfc059d4719f606ab7b6e3e6d53ad
tchen <tyr.chen@gmail.com>
2014-01-23 14:25:30 +0800

    make the client code more flexible

--- webclient/assets/js/coderunner.js
+++ webclient/assets/js/coderunner.js
@@ -1,41 +1,72 @@
-function CodeRunner(wsuri, parent) {
-    var sock = null;
-
-    initWebsocket();
-
-    var $coderunner = $('.coderunner');
-    var $parent;
-
-    $coderunner.on('click', function(e) {
-        $parent = $(this).closest(parent); 
-        var $content = $('.coderunner-content', $parent);
-        var suffix = $coderunner.attr('suffix');
-        var content = $content.text() || $content.val();
-        send({'Suffix': suffix, 'Content': content});
-        return false;
-    });
-    
-    function send(data) {
-        var msg = JSON.stringify(data);
-        sock.send(msg);
-
-    };
-
-    function initWebsocket() {
-        sock = new ReconnectingWebSocket(wsuri);
-
-        sock.onopen = function() {
-            console.log("connected to " + wsuri);
-        }
-
-        sock.onclose = function(e) {
-            console.log("connection closed (" + e.code + ")");
-        }
-
-        sock.onmessage = function(e) {
-            console.log("message received: " + e.data);
-            var data = JSON.parse(e.data);
-            $('.coderunner-result', $parent).text(data.Content);
-        }
+(function ( $ ) {
+    var defaults = {
+        wsuri: '',
+        button: '<button class="coderunner-btn btn btn-default">Run!</button>',
+        result: '<hr/><pre class="result"><code class="coderunner-result hljs ">Code execution result</code></pre>',
+        buttonSelector: '.coderunner-btn',
+        resultSelector: '.coderunner-result',
+        contentSelector: '.coderunner-content',
+        parentSelector: 'div.coderunner',
+        buttonContainer: 'pre',
+        codeContainer: 'code'
     }
-}
+
+    var attached = false;
+
+
+    
+    $.CodeRunner  = function(options) {
+        $.extend(defaults, options);       
+    }
+
+    $.fn.CodeRunner = function() {
+        if (attached) return this;
+
+        var $parent, $btn;
+        var sock = null;
+
+        // append nodes
+        this.filter(defaults.parentSelector).append(defaults.result);
+        $(defaults.buttonContainer, this).append(defaults.button);
+        $(defaults.codeContainer, this).addClass(defaults.contentSelector)
+
+        $btn = $(defaults.buttonSelector);
+
+        $btn.on('click', function() {
+            $parent = $(this).closest(defaults.parentSelector); 
+            var $content = $(defaults.contentSelector, $parent);
+            var suffix = $content.attr('suffix');
+            var content = $content.text() || $content.val();
+            send({'Suffix': suffix, 'Content': content});
+            return false;            
+        });
+
+        initWebsocket(defaults.wsuri);
+
+        function send(data) {
+            var msg = JSON.stringify(data);
+            sock.send(msg);
+        };
+
+        function initWebsocket(wsuri) {
+            sock = new ReconnectingWebSocket(defaults.wsuri);
+
+            sock.onopen = function() {
+                console.log("connected to " + wsuri);
+            }
+
+            sock.onclose = function(e) {
+                console.log("connection closed (" + e.code + ")");
+            }        
+
+            sock.onmessage = function(e) {
+                console.log("message received: " + e.data);
+                var data = JSON.parse(e.data);
+                $(defaults.resultSelector, $parent).text(data.Content);
+            }
+        }
+        return this;
+    };
+ 
+    
+}( jQuery ));
